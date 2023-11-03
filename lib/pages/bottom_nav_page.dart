12a462b85e5c8575cc9dc817ef79903eb51e3beb
hoesk1ee <hoeskiee@gmail.com>
2023-11-03 22:49:35 +0700

    Add drawer

--- lib/pages/bottom_nav_page.dart
+++ lib/pages/bottom_nav_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:badges/badges.dart' as badges;
+import 'package:gap/gap.dart';
 import 'package:unjuk_keterampilan_fg/pages/call_page.dart';
 import 'package:unjuk_keterampilan_fg/pages/chat_page.dart';
 import 'package:unjuk_keterampilan_fg/pages/people_page.dart';
@@ -127,16 +128,31 @@ class _MainAppState extends State<MainApp> {
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: ColorPalette().iconChatColor,
         unselectedItemColor: ColorPalette().unselectedColor,
-        items: const [
+        items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble),
+            icon: badges.Badge(
+              badgeStyle: badges.BadgeStyle(
+                borderSide:
+                    BorderSide(color: ColorPalette().mainWhiteColor, width: 3),
+              ),
+              badgeContent: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Text(
+                  "3",
+                  style: TextStyle(
+                      color: ColorPalette().mainWhiteColor, fontSize: 11),
+                ),
+              ),
+              position: badges.BadgePosition.topEnd(top: -10, end: -8),
+              child: const Icon(Icons.chat_bubble),
+            ),
             label: "Chats",
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.videocam),
             label: "Calls",
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.people),
             label: "People",
           ),
@@ -146,7 +162,120 @@ class _MainAppState extends State<MainApp> {
       ),
       drawer: const NavigationDrawer(
         children: [
-          Text('Drawer'),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        'https://lh3.googleusercontent.com/4RtSy8rScY9INrYqtibqVeA_GvzE-n-oufMTGASBM0xnCzRGC2O9KFgYPUv9vaoJrWvIq-m9Db4k4CYj9fExJVzuAayfQOsftnH5rBI1tqRA3_do=w960-rj-nu-e365',
+                      ),
+                    ),
+                    Gap(16),
+                    Text("Ferry Gunawan"),
+                    Icon(Icons.expand_more),
+                    Spacer(),
+                    Icon(Icons.settings),
+                  ],
+                ),
+                Gap(32),
+                DrawerListItem(
+                  isSelected: true,
+                  label: 'Chats',
+                  icons: Icons.chat_bubble,
+                ),
+                Gap(8),
+                DrawerListItem(
+                  isSelected: false,
+                  label: 'Marketplace',
+                  icons: Icons.store,
+                ),
+                Gap(8),
+                DrawerListItem(
+                  isSelected: false,
+                  label: 'Message requests',
+                  icons: Icons.sms,
+                ),
+                Gap(8),
+                DrawerListItem(
+                  isSelected: false,
+                  label: 'Archive',
+                  icons: Icons.inventory,
+                ),
+                Gap(8),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DrawerListItem extends StatelessWidget {
+  const DrawerListItem({
+    super.key,
+    required this.label,
+    required this.icons,
+    required this.isSelected,
+  });
+
+  final String label;
+  final IconData icons;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: isSelected
+            ? ColorPalette().iconBackgroundColor
+            : Colors.transparent,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: ColorPalette().unselectedColor.withOpacity(0.2),
+            ),
+            child: Icon(
+              icons,
+              size: 20,
+            ),
+          ),
+          const Gap(16),
+          Text(
+            label,
+            style: const TextStyle(fontSize: 18),
+          ),
+          const Spacer(),
+          isSelected
+              ? badges.Badge(
+                  badgeStyle: badges.BadgeStyle(
+                    borderRadius: BorderRadius.circular(12),
+                    shape: badges.BadgeShape.square,
+                    badgeColor: ColorPalette().iconChatColor,
+                  ),
+                  badgeContent: Padding(
+                    padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+                    child: Text(
+                      "3",
+                      style: TextStyle(
+                        color: ColorPalette().mainWhiteColor,
+                        fontSize: 11,
+                      ),
+                    ),
+                  ),
+                )
+              : const Text("")
         ],
       ),
     );
